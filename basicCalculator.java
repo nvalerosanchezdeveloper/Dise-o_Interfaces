8929cec6bef0321b5767244fff7c71763d5b533b
--Efecto-- <nvalerosanchezdeveloper@gmail.com>
2026-09-17 21:12:57 +0200

    Procrastinación Activa
    Lo puse más bonito mientras maduraba la lógica que quería aplicar.

--- basicCalculator.java
+++ basicCalculator.java
@@ -4,29 +4,34 @@ import java.util.Scanner;
 public class Main {
     public static void main (String[] args){
 
-        System.out.printf("FOR USERS:\n 
-        --- OPTIONS ARE SET AS NUMBERS FROM 1 - 5 ---\n
-        OPTION 1: Addition (+)\n
-        OPTION 2: Substraction (-)\n
-        Option 3: Multiplication (*)\n
-        Option 4: Division (/)\n
-        Option 5: End\n");
-        scanner();
+        char [] operators = {'+','-','*','/','#'};
 
+        System.out.printf("[--------------=== FOR USERS ===--------------]\n\n" +
+                        "|--- OPTIONS ARE SET AS NUMBERS FROM 1 - 5 ---|\n" +
+                        "|---        OPTION 1: Addition (+)         ---|\n" +
+                        "|---        OPTION 2: Substraction (-)     ---|\n" +
+                        "|---        Option 3: Multiplication (*)   ---|\n" +
+                        "|---        Option 4: Division (/)         ---|\n" +
+                        "|---        Option 5: End (#)              ---|\n" );
+                        scanner();
     }
 
+    //region Lectura
     public static void scanner(){
         Scanner sc = new Scanner(System.in);
 
         System.out.println();
+        System.out.print("Please, choose an operation ---> ");
+        int option = sc.nextInt();
 
-        System.out.println("Enter first number ---> ");
+        System.out.print("Enter first number ---> ");
         int a = sc.nextInt();
 
-        System.out.println("Enter second number ---> ");
+        System.out.print("Enter second number ---> ");
         int b = sc.nextInt();
-
-        System.out.println("Choose an operation ---> ");
-
     }
+    //endregion
+
+
+
 }
