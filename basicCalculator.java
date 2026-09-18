2425f71bff4788da2a397b571ddffa5cea68f0be
--Efecto-- <nvalerosanchezdeveloper@gmail.com>
2026-09-18 18:27:52 +0200

    Volvemos a la carga
    Le he añadido la opción de la resta, quedan 3 carácteres. Me he dado cuenta de qu eel bucle sólo recurre a pedir los números a calcular pero, una vez escogido el operador, no repite el proceso entero, redunda en la misma operación.

--- basicCalculator.java
+++ basicCalculator.java
@@ -2,6 +2,72 @@
 // que me estaba gustando, perdón.
 
 import java.util.Scanner;
+//Quería usar equals, pero descubrí que no existía para los char y quise priorizar hacer el ejercicio enrevesado,
+// que me estaba gustando, perdón.
+
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main (String[] args){
+
+        char [] operators = {'+','-','*','/','#'};
+
+        System.out.printf("[--------------=== FOR USERS ===--------------]\n\n" +
+                        "|--- OPTIONS ARE SET AS NUMBERS FROM 1 - 5 ---|\n" +
+                        "|---        OPTION 1: Addition (+)         ---|\n" +
+                        "|---        OPTION 2: Substraction (-)     ---|\n" +
+                        "|---        Option 3: Multiplication (*)   ---|\n" +
+                        "|---        Option 4: Division (/)         ---|\n" +
+                        "|---        Option 5: End (#)              ---|\n" );
+
+        char option = scannerOperators();
+        comparator (option);
+    }
+
+
+    //region Lectura
+    public static char scannerOperators(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println();
+        System.out.print("Please, choose an operation ---> ");
+        char option = sc.next().charAt(0);
+        return option;
+    }
+
+    public static int [] scannerNumbers(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number ---> ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number ---> ");
+        int b = sc.nextInt();
+
+        return new int[] {a,b};
+    }
+
+
+    //endregion
+
+    //region Comparador
+    public static void comparator(char option){
+        while(option != '#'){
+            if (option == '+'){
+                int [] numbers = scannerNumbers();
+                System.out.println(numbers[0]+ "+" + numbers [1] + "= " + (numbers[0]+ numbers[1]));
+            } else if (option == '-'){
+              int[] numbers = scannerNumbers();
+              System.out.println( numbers[0] + "-" + numbers [1] + "= " +(numbers[0] - numbers[1]));
+            }
+        }
+    }
+    //endregion
+
+
+
+}
 
 
 public class Main {
